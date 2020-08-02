f2acc58221b6302f4311e567149fb6788aa01da9
Alimammadov <eli_memmedli@hotmail.com>
2020-08-02 17:57:10 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 
-var alphabet = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-var numbers = [1,2,3,4,5,6,7,8,9,0],
+const alphabet = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = [1,2,3,4,5,6,7,8,9,0];
 
 
-var uid = '';
+let uid = '';
 
 
 
-
-var a = alphabet[Math.floor(Math.random() * alphabet.length)];
-var b = numbers[Math.floor(Math.random() * numbers.length)];
-var c = alphabet[Math.floor(Math.random() * alphabet.length)];
-var d = alphabet[Math.floor(Math.random() * alphabet.length)];
-var f= numbers[Math.floor(Math.random() * numbers.length)];
-var g = alphabet[Math.floor(Math.random() * alphabet.length)];
+//If you want to change UID length you can add here. This example has 6 symbol
+let a = alphabet[Math.floor(Math.random() * alphabet.length)];
+let b = numbers[Math.floor(Math.random() * numbers.length)];
+let c = alphabet[Math.floor(Math.random() * alphabet.length)];
+let d = alphabet[Math.floor(Math.random() * alphabet.length)];
+let f= numbers[Math.floor(Math.random() * numbers.length)];
+let g = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 
 console.log(a+b+c+d+f+g)
 
 uid = a+b+c+d+f+g;
 
-document.getElementById("demo").innerHTML = "Order number: " + uid;
+document.getElementById("orderId").innerHTML = "Order number: " + uid;
 
